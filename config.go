f0a7b5a7982d5fa3851d94c59ea433708ec3f026
unknown <shh.im.coding@gmail.com>
2019-07-22 14:09:54 -0300

    config done

--- config.go
+++ config.go
@@ -9,7 +9,7 @@ type configModel struct {
 }
 
 var config = configModel{
-	mongoUri:    "mongodb://admin:123456@localhost:37812/react-recipes", // Mongo Uri example: mongodb://admin:123456@localhost:37812/react-recipes
+	mongoUri:    "mongodb://admin:Damba11a@ds253567.mlab.com:53567/react-recipes", // Mongo Uri example: mongodb://admin:123456@localhost:37812/react-recipes
 	mongoDb:     "react-recipes",                                        // DB name
 	tokenSecret: "secret",                                               // Secret to use in Tokens
 	tokenExp:    "1h",                                                   // Expiration of Token
